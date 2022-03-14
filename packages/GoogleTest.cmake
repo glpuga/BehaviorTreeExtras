f83f7a0e27e637d6377a5e14959362df0ed4d372
Gerardo Puga <glpuga@ekumenlabs.com>
2022-03-14 14:37:00 -0300

    Fix that the checkout of test in isolated mode is shallow, and therefore cannot checkout anything but GTest's head (#6)

--- packages/GoogleTest.cmake
+++ packages/GoogleTest.cmake
@@ -2,7 +2,6 @@ include(FetchContent)
 FetchContent_Declare(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
   GIT_TAG           c9461a9b55ba954df0489bab6420eb297bed846b    # tag: v1.10.0
-  GIT_SHALLOW       true
   GIT_PROGRESS      true
 )
 
